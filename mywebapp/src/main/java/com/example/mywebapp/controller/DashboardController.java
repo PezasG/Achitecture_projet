cb91ce0a2c525f9cc6886516aee69f83204552fe
Gaëtan Pezas <gaetandestructeur@gmail.com>
2025-11-23 21:39:26 +0100

    Webapp démarre enfin !

--- mywebapp/src/main/java/com/example/mywebapp/controller/DashboardController.java
+++ mywebapp/src/main/java/com/example/mywebapp/controller/DashboardController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
-    // Quand tu vas sur http://localhost:8081/login
-    // → cela affiche le fichier templates/login.html
     @GetMapping("/dashboard_employe")
     public String showLoginPage() {
         return "dashboard_employe"; // correspond à templates/login.html
--- mywebapp/src/main/java/com/example/mywebapp/model/Employee.java
+++ mywebapp/src/main/java/com/example/mywebapp/model/Employee.java
@@ -4,11 +4,13 @@ import java.util.Date;
 
 public class Employee {
     private Long id;
+
     private String FirstName;
     private String LastName;
-
     private String email;
     private String job;
+    private int hours;
+    private int salary;
 
     // Getters et Setters
 
@@ -51,4 +53,12 @@ public class Employee {
     public void setJob(String job) {
         this.job = job;
     }
+
+    public int getHours() { return hours; }
+
+    public void setHours(int hours) { this.hours = hours; }
+
+    public int getSalary() { return salary; }
+
+    public void setSalary(int salary) { this.salary = salary; }
 }
